73c314edb358a3331012cef32171becb40091c3f
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-28 21:51:45 +0800

    feat: Add lazy loading for images in Experiences and Projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,28 @@
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import { PROJECTS } from '../constants';
+import { motion } from "framer-motion"
 
 const Projects = () => {
     return (
         <div className=" pb-4">
-            <h1 className="my-20 text-center text-4xl">Projects</h1>
+            <motion.h1
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-20 text-center text-4xl"
+            >
+                Projects
+            </motion.h1>
 
             <div>
                 {PROJECTS.map((project, index) => (
                     <div key={index} className="my-20 mb-10 flex flex-wrap lg:justify-center">
-                        <div className="w-full lg:w-1/4">
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full lg:w-1/4"
+                        >
                             <img
                                 src={project.image}
                                 alt={project.name}
@@ -17,8 +30,13 @@ const Projects = () => {
                                 width={250}
                                 height={135}
                             />
-                        </div>
-                        <div className="w-full max-w-xl lg:w-3/4">
+                        </motion.div>
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full max-w-xl lg:w-3/4"
+                        >
                             <h6 className="mb-2 font-semibold">
                                 {project.title}
                             </h6>
@@ -28,13 +46,21 @@ const Projects = () => {
                                     {technology}
                                 </span>
                             ))}
-                        </div>
+                        </motion.div>
                     </div>
                 ))}
             </div>
-            <div className="flex flex-wrap justify-center gap-8">
+            <div
+
+                className="flex flex-wrap justify-center gap-8"
+            >
                 {PROJECTS.map((project, index) => (
-                    <div key={index} className="lg:mt-14 lg:mb-14 relative max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"> {/* Add mb-10 for margin bottom */}
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ type: "spring", stiffness: 200, damping: 9 }}
+                        key={index}
+                        className="lg:mt-14 lg:mb-14 relative max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                    > {/* Add mb-10 for margin bottom */}
                         <div className="p-5 pb-16"> {/* Add padding bottom to make space for the absolute positioned elements */}
                             <a href="#">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.title}</h5>
@@ -58,7 +84,7 @@ const Projects = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
