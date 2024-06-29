fcd0ad7657b9ea18c6d926bdad9299a083d74e97
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-29 14:42:36 +0800

    feat: Add social media links with motion effects to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/kevinRushLogo.png'
 import { FaLinkedin, FaGithub, FaInstagram, FaFilePdf } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
     return (
@@ -8,9 +9,27 @@ const Navbar = () => {
                 <img className='mx-4 w-10' src={logo} alt="Logo" />
             </div>
             <div className="mx-3 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin className="cursor-pointer" />
-                <FaGithub className="cursor-pointer" />
-                <FaInstagram className="cursor-pointer" />
+                <motion.a
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    href="https://www.linkedin.com/in/aimannaimfaizul/"
+                >
+                    <FaLinkedin className="cursor-pointer" />
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    href="https://github.com/aimxnaim"
+                >
+                    <FaGithub className="cursor-pointer" />
+                </motion.a>
+                <motion.a
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    href="https://www.instagram.com/aimxnaim/"
+                >
+                    <FaInstagram className="cursor-pointer" />
+                </motion.a>
                 <FaFilePdf className="cursor-pointer" />
             </div>
         </nav>
