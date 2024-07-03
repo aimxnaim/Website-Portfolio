cd6706bf03243a3f7333bf7906decac5383b3eee
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-07-03 21:28:50 +0800

    chore: Update npm dependencies and add @vercel/analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Technology from './components/Technology'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
           <Experiences />
           <Projects />
           <Footer />
+          <Analytics />
 
         </div>
       </div>
